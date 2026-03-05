b669474834b5ae793c7d26931d03a3a583316437
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-05 13:06:57 +0100

    dodanie pliku env.ts

---
+++ utils/env.ts
@@ -0,0 +1,8 @@
+export const ENV = {
+
+  baseUrl: 'https://wyszukiwarkaregon.stat.gov.pl/appBIR/index.aspx',
+
+  apiUrl:
+    'https://wyszukiwarkaregon.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc/ajaxEndpoint/daneSzukaj'
+
+};
