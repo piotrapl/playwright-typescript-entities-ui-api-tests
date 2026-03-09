6fa7733406f51aa94642cd4e6e1e38889db6a157
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-09 10:41:07 +0100

    zmiany dot. raportowania Allure, zapisywanie screenów, dodanie wyłączeń

--- fixtures/test-fixtures.ts
+++ fixtures/test-fixtures.ts
@@ -1,4 +1,5 @@
 import { test as base } from '@playwright/test';
+import { allure } from 'allure-playwright';
 
 import { RegonPage } from '../pages/regon.page';
 import { RegonFlow } from '../flows/regon.flow';
@@ -8,15 +9,29 @@ type Fixtures = {
 };
 
 export const test = base.extend<Fixtures>({
-
   regonFlow: async ({ page }, use) => {
 
     const regonPage = new RegonPage(page);
-
     const regonFlow = new RegonFlow(regonPage);
 
     await use(regonFlow);
 
   }
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+
+  const screenshot = await page.screenshot();
+
+  await testInfo.attach('screenshot', {
+    body: screenshot,
+    contentType: 'image/png'
+  });
+
+  await allure.attachment(
+    'screenshot',
+    screenshot,
+    'image/png'
+  );
 
 });
